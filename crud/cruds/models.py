d9ae725f66a5516777a2cd6829668955563e5368
Vibhanshu <vibhanshujaiswal28@gmail.com>
2024-07-14 21:18:54 +0530

    updated

--- crud/cruds/models.py
+++ crud/cruds/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 
-class crud(models.Model):
+class detail(models.Model):
     name = models.CharField(max_length=60)
     branch = models.CharField(max_length=50)
     section = models.CharField(max_length=5)
     year = models.IntegerField()
     dob = models.DateField()
-    
+    email_id = models.EmailField()
+    password = models.CharField(max_length=20)
+
+
