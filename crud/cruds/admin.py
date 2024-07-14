e18808e6e8c635a9ffdeca0923b9f878c4287145
Vibhanshu <vibhanshujaiswal28@gmail.com>
2024-07-14 21:22:36 +0530

    updated

--- crud/cruds/admin.py
+++ crud/cruds/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from cruds.models import detail
 
 class details:
-    data = ()
+    data = ('name', 'branch', 'section', 'year', 'dob', 'email_id', 'password')
 
 admin.site.register(detail, details)
