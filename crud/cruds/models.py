05b51ce95237194636cdec19590f903e9710a17d
Vibhanshu <vibhanshujaiswal28@gmail.com>
2024-07-14 21:10:38 +0530

    updated

--- crud/cruds/models.py
+++ crud/cruds/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class crud(models.Model):
     name = models.CharField(max_length=60)
-    dob = models.DateField()
-    year = models.IntegerField()
     branch = models.CharField(max_length=50)
+    section = models.CharField(max_length=5)
+    year = models.IntegerField()
+    dob = models.DateField()
     
