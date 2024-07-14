3641fdc4d712f96e2cea9d944dbdc4d492188997
Vibhanshu <vibhanshujaiswal28@gmail.com>
2024-07-14 23:27:46 +0530

    updated

--- crud/crud/views.py
+++ crud/crud/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 from cruds.models import detail
 
 def home(request):
-
+    data = request.POST
+    if request.method == 'POST':
+        name = data.get('name')
+        email_id = data.get('email')
+        branch = data.get('branch')
+        section = data.get('section')
+        year = data.get('year')
+        dob = data.get('dob')
+        ob = detail(name=name, email_id=email_id, branch=branch, section=section, year=year, dob=dob)
+        ob.save()
     return render(request, 'home.html')
 
 
 def detail_page(request):
     ob = detail.objects.all()
     if ob:
+        x=[]
         for i in ob:
-            print(i)
-    return render(request, 'detail_page.html')
+            x.append(i)
+    return render(request, 'detail_page.html', {'key':x})
